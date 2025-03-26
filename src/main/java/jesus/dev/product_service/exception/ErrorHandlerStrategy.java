153574ca90141f4c592b729ac59024d5790bb215
Christian <jesus.chumpitaz@vallegrande.edu.pe>
2025-03-26 15:32:53 -0500

    fix(Main): Clean main branch

--- src/main/java/jesus/dev/product_service/exception/ErrorHandlerStrategy.java
+++
@@ -1,9 +0,0 @@
-package jesus.dev.product_service.exception;
-
-import jesus.dev.product_service.exception.model.ErrorResponse;
-
-public interface ErrorHandlerStrategy {
-    boolean supports(Throwable error);
-
-    ErrorResponse handle(Throwable error);
-}
--- src/test/java/jesus/dev/product_service/ProductServiceApplicationTests.java
+++
@@ -1,7 +0,0 @@
-package jesus.dev.product_service;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProductServiceApplicationTests {
-}
